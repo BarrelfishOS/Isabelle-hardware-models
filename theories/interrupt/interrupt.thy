7373ce717cef7a579ec40f47bd3e9ce7c18a5fd6
Lukas Humbel <lukas.humbel@inf.ethz.ch>
2018-01-15 14:01:50 +0100

    Getting rid of annoying option datatype
    Signed-off-by: Lukas Humbel <lukas.humbel@inf.ethz.ch>

--- theories/interrupt/interrupt.thy
+++ theories/interrupt/interrupt.thy
@@ -63,19 +63,20 @@ record CONTROLLER =
 
 definition replace_map :: "CONTROLLER \<Rightarrow> IRQ \<Rightarrow> IRQ set \<Rightarrow> CONTROLLER"
   where "replace_map orig new_inp new_out = orig \<lparr> map := (\<lambda>i. (if i = new_inp then new_out else (map orig) i)) \<rparr>"  
+
+definition replace_map_ctrl :: "(CONTROLLER \<times> CONTROLLER_CLASS) \<Rightarrow> IRQ \<Rightarrow> IRQ set \<Rightarrow> (CONTROLLER \<times> CONTROLLER_CLASS)"
+  where "replace_map_ctrl orig new_inp new_out = (replace_map (fst orig) new_inp new_out, snd orig)"  
  
 (* Interconnect of the system + each controller with a class *)
 record SYSTEM =
-  controller :: "CONTROLLER_NAME \<Rightarrow> (CONTROLLER \<times> CONTROLLER_CLASS) option"
+  controller :: "CONTROLLER_NAME \<Rightarrow> (CONTROLLER \<times> CONTROLLER_CLASS)"
   net :: "CONTROLLER_NAME \<Rightarrow> nat \<Rightarrow> (CONTROLLER_NAME \<times> nat) set"
 
 (* In system at controller name, replace its mapping from new_inp to new_outp *)
 definition replace_system :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> IRQ \<Rightarrow> IRQ set \<Rightarrow> SYSTEM"
   where "replace_system orig name new_inp new_out = orig \<lparr>
-    net := (net orig),
-    controller := (\<lambda>c. (if c = name then (
-                   (case ((controller orig) c) of Some cr \<Rightarrow> Some (replace_map (fst cr) new_inp new_out, snd cr) | _ \<Rightarrow> None)
-                ) else (controller orig) c))\<rparr>"
+      controller := (controller orig)(name := replace_map_ctrl ((controller orig)name) new_inp new_out)
+    \<rparr>"
   
 (* look at decoding net to verify *)
 (* assuming system is valid, configuration update \<Rightarrow> system valid *)  
@@ -90,24 +91,20 @@ definition doms :: "('a \<Rightarrow> 'b set) \<Rightarrow> 'a set" where
   "doms m = {a. m a \<noteq> {}}"
   
 (* Get  the actual configuration of a controller name *)   
-definition sys_ctrl_act_conf :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> (IRQ \<Rightarrow> IRQ set) option"
-  where "sys_ctrl_act_conf s c = (case controller s c of None \<Rightarrow> None |
-         Some the_c \<Rightarrow> Some (map (fst the_c)))"
+definition sys_ctrl_act_conf :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> (IRQ \<Rightarrow> IRQ set)"
+  where "sys_ctrl_act_conf s cn = map (fst ((controller s) cn))"
    
 (* Get the set valid configuration of a controller name *)    
-definition sys_ctrl_valid_conf :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> ((IRQ \<Rightarrow> IRQ set) set) option"
-  where "sys_ctrl_valid_conf s c = (case controller s c of None \<Rightarrow> None | 
-         Some the_c \<Rightarrow> Some (map_valid (snd the_c)))"
+definition sys_ctrl_valid_conf :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> ((IRQ \<Rightarrow> IRQ set) set)"
+  where "sys_ctrl_valid_conf s cn = (map_valid (snd ((controller s) cn)))"
     
 (* Check if c has a valid config in s *)
 definition sys_ctrl_valid :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> bool"
-  where "sys_ctrl_valid s c = (case (sys_ctrl_act_conf s c) of None \<Rightarrow> True | Some conf \<Rightarrow> 
-    case (sys_ctrl_valid_conf s c) of Some valid_confs \<Rightarrow> conf \<in> valid_confs | False)"
+  where "sys_ctrl_valid s cn = ((sys_ctrl_act_conf s cn) \<in> (sys_ctrl_valid_conf s cn))"
  
 (* Check if a system is in a valid state *)
-(* valid system = all ports wired, all controller in valid state + ...*)
 definition sys_valid :: "SYSTEM \<Rightarrow> bool"
-  where "sys_valid s = (\<forall> c. c \<in> (dom (controller s)) \<longrightarrow> sys_ctrl_valid s c)"
+  where "sys_valid s = (\<forall> c. sys_ctrl_valid s c)" 
 
 
 (* lift to decoding net, assuming valid *)
@@ -117,23 +114,23 @@ definition irq_format_nat_encode :: "FORMAT \<Rightarrow> nat" where
   "irq_format_nat_encode x = (case x of FINVALID \<Rightarrow> prod_encode (0,0) | FEMPTY \<Rightarrow> prod_encode (1,0)
  | FVECTOR a  \<Rightarrow> prod_encode (2,a) | FMEMWRITE a b \<Rightarrow> prod_encode (3, prod_encode (a,b) ))"
 
-definition irq_format_nat_decode :: "nat \<Rightarrow> FORMAT option" where
-  "irq_format_nat_decode x = (if fst (prod_decode x) = 0 then Some FINVALID else if 
-   fst (prod_decode x) = 1 then Some FEMPTY else if fst (prod_decode x) = 2 then Some (FVECTOR (snd (prod_decode x))) else if
-   fst (prod_decode x) = 3 then Some (FMEMWRITE (fst (prod_decode (snd (prod_decode x)))) (snd (prod_decode (snd (prod_decode x))))) else None)"
+(* If it can't be mapped, it will return FINVALID *)
+definition irq_format_nat_decode :: "nat \<Rightarrow> FORMAT" where
+  "irq_format_nat_decode x = ( if 
+   fst (prod_decode x) = 1 then FEMPTY else if fst (prod_decode x) = 2 then (FVECTOR (snd (prod_decode x))) else if
+   fst (prod_decode x) = 3 then (FMEMWRITE (fst (prod_decode (snd (prod_decode x)))) (snd (prod_decode (snd (prod_decode x))))) else FINVALID)"
 
-lemma format_enc_inv: "irq_format_nat_decode(irq_format_nat_encode x) = Some x"
+lemma format_enc_inv: "irq_format_nat_decode(irq_format_nat_encode x) = x"
   apply(simp add:irq_format_nat_encode_def add:irq_format_nat_decode_def)
   by (smt FORMAT.exhaust FORMAT.simps(15) FORMAT.simps(16) FORMAT.simps(17) FORMAT.simps(18) fst_conv numeral_1_eq_Suc_0 numeral_One numeral_eq_iff prod_encode_inverse semiring_norm(83) semiring_norm(86) semiring_norm(89) snd_conv zero_neq_numeral)
-
+  
 definition irq_nat_encode :: "IRQ \<Rightarrow> nat" where
   "irq_nat_encode i = prod_encode ((irq_format_nat_encode (format i)), port i)"
 
-definition irq_nat_decode :: "nat \<Rightarrow> IRQ option" where
-  "irq_nat_decode n = (case (irq_format_nat_decode (fst (prod_decode n))) of (Some fmt) \<Rightarrow> (Some \<lparr>format = fmt,
-   port = (snd (prod_decode n)) \<rparr> ) | None \<Rightarrow> None)"
+definition irq_nat_decode :: "nat \<Rightarrow> IRQ" where
+  "irq_nat_decode n = \<lparr>format = irq_format_nat_decode (fst (prod_decode n)), port = (snd (prod_decode n)) \<rparr>"
 
-lemma irq_enc_inv: "irq_nat_decode(irq_nat_encode x) = Some x"
+lemma irq_enc_inv: "irq_nat_decode(irq_nat_encode x) = x"
   by(simp add:irq_nat_encode_def add:irq_nat_decode_def add:format_enc_inv)
 
 
@@ -147,27 +144,19 @@ definition irq_set_to_name_set :: "(CONTROLLER_NAME \<times> IRQ) set \<Rightarr
 
 (* Apply net translation, then translate to names *)
 definition conf_to_translate_mod_conf :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> (IRQ \<Rightarrow> IRQ set) \<Rightarrow> IRQ \<Rightarrow> name set"
-  where "conf_to_translate_mod_conf s c conf i = irq_set_to_name_set (net_set_translate s c (conf i))"
+  where "conf_to_translate_mod_conf s cn conf i = irq_set_to_name_set (net_set_translate s cn (conf i))"
 
-(* get rid of option *)
-definition conf_to_translate_mod_c :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> IRQ \<Rightarrow> name set"
-  where "conf_to_translate_mod_c s c i = (case ((controller s) c) of Some ctrl \<Rightarrow> conf_to_translate_mod_conf s c (map (fst ctrl)) i |
-    None \<Rightarrow> {})"
-
-(* get rid of option *)
-definition conf_to_translate_mod :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> IRQ option \<Rightarrow> name set"
-  where "conf_to_translate_mod s c ini = (case ini of Some i \<Rightarrow>
-            conf_to_translate_mod_c s c i
-         | None \<Rightarrow> {})"
+definition conf_to_translate_mod :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> IRQ \<Rightarrow> name set"
+  where "conf_to_translate_mod s cn ini = 
+            irq_set_to_name_set (net_set_translate s cn ((sys_ctrl_act_conf s cn) ini))"
 
 definition conf_to_translate :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> (addr \<Rightarrow> name set)"
-  where "conf_to_translate s c = (case ((controller s) c) of Some ctrl \<Rightarrow>
-            (\<lambda>addr. conf_to_translate_mod s c (irq_nat_decode addr))
-         | None \<Rightarrow> (\<lambda>_. {}))"
+  where "conf_to_translate s cn = 
+            (\<lambda>addr. conf_to_translate_mod s cn (irq_nat_decode addr))
+         "
     
 definition mk_node :: "SYSTEM \<Rightarrow> CONTROLLER_NAME \<Rightarrow> node"
-  where "mk_node s c = (case (sys_ctrl_act_conf s c) of None \<Rightarrow> empty_node |
-         Some conf \<Rightarrow> \<lparr>accept = {}, translate = conf_to_translate s c \<rparr>)"
+  where "mk_node s cn = \<lparr>accept = {}, translate = conf_to_translate s cn \<rparr>"
 
 (* ------------------------------------------------------------------------- *)  
 subsection "Lifting Result"
@@ -196,7 +185,7 @@ definition replace_node :: "node \<Rightarrow> addr \<Rightarrow> name set \<Rig
    ctrl is a nat, that represents the controller id in the interrupt net and in the decoding net  *)
 lemma lifts: 
   (* ctrl exists *)
-  assumes ctrl_exists: "((controller sys) ctrl) = Some ctrlx" 
+  assumes ctrl_exists: "((controller sys) ctrl) = ctrlx" 
   (* No mapping for inirq at ctrl exists *)
   and no_init_mapping: "(map (fst ctrlx)) inirq = {}"         
   (* Assume an identity net function *)
@@ -208,9 +197,7 @@ proof -
   show ?thesis
     apply(simp_all add:ctrl_exists add:no_init_mapping add:ident_net add:replace_system_def
      add:replace_node_def add:mk_node_def add:replace_map_def  add:sys_ctrl_act_conf_def
-     add:conf_to_translate_def add:conf_to_translate_mod_def add:conf_to_translate_mod_c_def)
-
-  
+     add:conf_to_translate_def add:conf_to_translate_mod_def)
 
 qed
   
